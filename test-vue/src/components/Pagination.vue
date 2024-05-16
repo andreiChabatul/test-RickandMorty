b463e0bed5c04b46a73865b705e4a704d72fdc16
andreiChabatul <andreichabatul@gmail.com>
2024-05-16 16:07:54 +0300

    feat component pagination

---
+++ test-vue/src/components/Pagination.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useStore } from '@/store';
+import Paginator, { type PageState } from 'primevue/paginator';
+
+const store = useStore();
+
+function setPage(event: PageState) {
+    store.commit('setPage', event.page + 1);
+    store.dispatch('fetchAPI');
+}
+
+</script>
+
+<template>
+    <div class="overflow-auto">
+        <Paginator @page="setPage" :rows="20" :totalRecords=store.state.totalRecords :currentPage=store.state.page>
+        </Paginator>
+    </div>
+</template>
